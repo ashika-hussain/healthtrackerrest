2ed24cb6552a37f9308e0652b44301f62ddfe8ed
ashika-hussain <ashikahussain111@gmail.com>
2023-10-23 12:59:32 +0100

    Add Activity and its CRUD operations

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -26,11 +26,17 @@ class JavalinConfig {
                     get(HealthTrackerController::getUserByUserId)
                     delete(HealthTrackerController::deleteUser)
                     patch(HealthTrackerController::updateUser)
+                    path("activities"){
+                        get(HealthTrackerController::getActivitiesByUserId)
+                    }
                 }
                 path("/email/{email}"){
                     get(HealthTrackerController::getUserByEmail)
                 }
-
+            }
+            path("/api/activities") {
+                get(HealthTrackerController::getAllActivities)
+                post(HealthTrackerController::addActivity)
             }
 
         }
