978a0417a99ad12a09dd30c1bf73c115d293f56a
ashika-hussain <ashikahussain111@gmail.com>
2023-11-03 00:27:25 +0000

    Add new CRUD operations in Activity

--- src/main/kotlin/ie/setu/domain/repository/ActivityDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/ActivityDAO.kt
@@ -3,9 +3,7 @@ package ie.setu.domain.repository
 import ie.setu.domain.Activity
 import ie.setu.domain.db.Activities
 import ie.setu.utils.mapToActivity
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class ActivityDAO {
@@ -40,14 +38,40 @@ class ActivityDAO {
     }
 
     //Save an activity to the database
-    fun save(activity: Activity){
-        transaction {
+    fun save(activity: Activity): Int{
+       return transaction {
             Activities.insert {
                 it[description] = activity.description
                 it[duration] = activity.duration
                 it[started] = activity.started
                 it[calories] = activity.calories
                 it[userId] = activity.userId
+            }
+        }get Activities.id
+    }
+
+    fun deleteByUserId (userId: Int): Int{
+        return transaction{
+            Activities.deleteWhere { Activities.userId eq userId }
+        }
+    }
+
+    fun deleteByActivityId (activityId: Int): Int{
+        return transaction{
+            Activities.deleteWhere { Activities.id eq activityId }
+        }
+    }
+
+
+    fun updateByActivityId(activityId: Int, activityDTO: Activity){
+        transaction {
+            Activities.update ({
+                Activities.id eq activityId}) {
+                it[description] = activityDTO.description
+                it[duration] = activityDTO.duration
+                it[started] = activityDTO.started
+                it[calories] = activityDTO.calories
+                it[userId] = activityDTO.userId
             }
         }
     }
